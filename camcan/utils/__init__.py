39a9bbfacfb1089279905cd2524ee8b68f2f23e7
Oleh Kozynets <ok7mailbox@gmail.com>
2019-03-04 15:45:19 +0100

    summary generation

--- camcan/utils/__init__.py
+++ camcan/utils/__init__.py
@@ -4,5 +4,6 @@ The :mod:`camcan.utils` module includes various utilities.
 
 from .atlas import make_masker_from_atlas
 from .shell import run_fs
+from .file_parsing import get_area, get_thickness
 
-__all__ = ['make_masker_from_atlas', 'run_fs']
+__all__ = ['make_masker_from_atlas', 'run_fs', 'get_area', 'get_thickness']
--- scripts/feature_extraction/extract_structural.py
+++ scripts/feature_extraction/extract_structural.py
@@ -4,15 +4,20 @@ from os.path import isdir, join
 
 import joblib
 from joblib import Parallel, delayed
+import numpy as np
+import pandas as pd
 
 from camcan.preprocessing import get_structural_data
+from camcan.utils import get_area, get_thickness
 
 # import pdb; pdb.set_trace()
+
 
 # test functions on MRI data
 CAMCAN_CONNECTIVITY = '/storage/data/camcan/camcan_connectivity'
 CAMCAN_FREESURFER = '/storage/store/data/camcan-mne/freesurfer'
 OUT_DIR = '/storage/tompouce/okozynet/camcan/structural'
+VOLUME_FILE = 'aseg.csv'
 N_JOBS = 10
 # list of subjects that we have connectivity data for
 subjects = [d[4:] for d in os.listdir(CAMCAN_CONNECTIVITY) if isdir(join(CAMCAN_CONNECTIVITY, d))]
@@ -20,3 +25,56 @@ subjects = [d[4:] for d in os.listdir(CAMCAN_CONNECTIVITY) if isdir(join(CAMCAN_
 structural_data = Parallel(n_jobs=N_JOBS, verbose=1)(
                            delayed(get_structural_data)(CAMCAN_FREESURFER, s, OUT_DIR)
                            for s in subjects)
+
+subjects = tuple(d for d in os.listdir(OUT_DIR) if isdir(join(OUT_DIR, d)))
+
+area_data = None
+thickness_data = None
+volume_data = None
+
+area_failed = []
+thickness_failed = []
+volume_failed = []
+
+for s in subjects:
+    subject_dir = join(OUT_DIR, s)
+    
+    try:
+        t_area = get_area(subject_dir)
+    except:
+        area_failed.append(s)
+
+    try:
+        t_thickness = get_thickness(subject_dir)
+    except:
+        volume_failed.append(s)
+
+    try:
+        volume = pd.read_csv(join(subject_dir, VOLUME_FILE), index_col=0)
+    except:
+        volume_failed.append(s)
+
+    if area_data is None:
+        area_data = pd.DataFrame(index=subjects, columns=np.arange(start=0, stop=len(t_area)), dtype=float)
+        area_data.loc[s] = t_area
+    else:
+        area_data.loc[s] = t_area
+    
+    if thickness_data is None:
+        thickness_data = pd.DataFrame(index=subjects, columns=np.arange(start=0, stop=len(t_thickness)), dtype=float)
+        thickness_data.loc[s] = t_thickness
+    else:
+        thickness_data.loc[s] = t_thickness
+
+    if volume_data is None:
+        volume_data = volume
+    else:
+        volume_data = pd.concat([volume_data, volume])
+
+print('Failed to load area data for\n', area_failed)
+print('Failed to load thickness data for\n', thickness_failed)
+print('Failed to load volume data for\n', volume_failed)
+
+area_data.to_csv(join(OUT_DIR, 'area_data.csv'))
+thickness_data.to_csv(join(OUT_DIR, 'thickness_data.csv'))
+volume_data.to_csv(join(OUT_DIR, 'volume_data.csv'))
