2816fd3b06e231069c4a54329956574abc429cc0
Oleh Kozynets <ok7mailbox@gmail.com>
2019-04-03 10:35:55 +0200

    MEG experiments

--- camcan/utils/__init__.py
+++ camcan/utils/__init__.py
@@ -7,10 +7,11 @@ from .shell import run_fs
 from .file_parsing import get_area, get_thickness
 from .notebook import (run_stacking, run_ridge, plot_pred, plot_error_scatters,
                        plot_learning_curve, plot_barchart, plot_boxplot, plot_error_age,
-                       plot_error_segments)
+                       plot_error_segments, run_meg_ridge)
 
 __all__ = ['make_masker_from_atlas', 'run_fs', 'get_area', 'get_thickness',
            'run_stacking','run_ridge', 'plot_pred',
            'plot_learning_curve', 'plot_barchart', 'plot_boxplot',
-           'plot_error_scatters', 'plot_error_age', 'plot_error_segments']
+           'plot_error_scatters', 'plot_error_age', 'plot_error_segments',
+           'run_meg_ridge']
 
