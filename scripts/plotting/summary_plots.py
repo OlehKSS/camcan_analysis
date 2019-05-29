435be5e9b628d91208b94fa5d7b7f35f86c78933
Oleh Kozynets <OlehKSS@users.noreply.github.com>
2019-05-29 14:33:05 +0200

    New plot, MAE vs age group (#13)

--- scripts/plotting/summary_plots.py
+++ scripts/plotting/summary_plots.py
@@ -14,31 +14,8 @@ OUT_FTYPE = 'png'
 
 out_folder = os.path.join(FIG_OUT_PATH, 'boxplots')
 
-if os.path.exists(out_folder):
-    shutil.rmtree(out_folder)
-    os.mkdir(out_folder)
-else:
-    os.mkdir(out_folder)
-
 all_regressions = pd.read_hdf(PREDICTIONS, key='regression')
 all_r2_scores = pd.read_hdf(PREDICTIONS, key='r2')
-
-# Plot summary boxplot
-keys = all_regressions.index
-
-# have I done boxplots right?
-
-title = 'Age Predictions, Combined'
-sns.set_style('darkgrid')
-plt.figure()
-ax = sns.boxplot(data=all_regressions.transpose(),
-                 showmeans=True,
-                 orient='h')
-ax.set_title(title)
-ax.set(xlabel='Absolute Prediction Error (Years)')
-
-name = f'combined_plot.{OUT_FTYPE}'
-plt.savefig(os.path.join(out_folder, name), bbox_inches='tight')
 
 # save mae, std to a csv file
 summary = pd.DataFrame(index=all_regressions.index,
@@ -47,3 +24,26 @@ summary['mae'] = all_regressions.mean(axis=1)
 summary['std'] = all_regressions.std(axis=1)
 summary['r2'] = all_r2_scores.mean(axis=1)
 summary.to_csv('../../data/summary.csv')
+
+# Plot summary boxplot
+keys = all_regressions.index
+
+plot_keys = (key.replace('Stacked-multimodal', 'Stacked') for key in keys)
+all_regressions.index = plot_keys
+
+title = 'Age Prediction'
+sns.set_style('whitegrid')
+plt.figure()
+ax = sns.boxplot(data=all_regressions.transpose(),
+                 showmeans=True,
+                 orient='h')
+ax.set_title(title)
+ax.set(xlabel='Mean Absolute Error (Years)')
+ax.spines['bottom'].set_color('black') 
+ax.spines['top'].set_color('black')  
+ax.spines['right'].set_color('black') 
+ax.spines['left'].set_color('black') 
+ax.tick_params(axis='x', colors='black', bottom=True)
+
+name = f'combined_plot.{OUT_FTYPE}'
+plt.savefig(os.path.join(out_folder, name), bbox_inches='tight')
