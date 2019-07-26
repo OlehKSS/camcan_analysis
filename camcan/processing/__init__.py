fc03f7ab06332f2d45580116d28248807ee5baf1
OlehKSS <OlehKSS@github.com>
2019-07-26 13:10:30 +0200

    Tangent space transfromation was added.

--- camcan/processing/__init__.py
+++ camcan/processing/__init__.py
@@ -2,6 +2,7 @@
 from .permutation_importance import permutation_importance
 from .spoc import SPoC
 from .stacking import StackingRegressor
+from .tangent import map_tangent
 
 __all__ = ['permutation_importance', 'StackingRegressor',
-           'SPoC']
+           'SPoC', 'map_tangent']
